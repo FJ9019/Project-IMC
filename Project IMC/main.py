10cc4cdd77aace563e9130c076a59daebbe8f00c
FJ9019 <feenlus24@gmail.com>
2025-02-07 11:58:48 -0500

    2nd Save

--- Project IMC/main.py
+++ Project IMC/main.py
@@ -14,7 +14,21 @@ def main():
     print("Bonjour M. Comment allez vous\n Veuillez vous connecté ! \n)Connexion\n2)Creer un compte")
     option = input(input(''))
     if option == 1:
+        users = load_data('users.json')
+        datas = load_data('datas.json')
         
+        for user in users :
+            print(f"{user['id']} - {user['nom']} {user['prenom']}")
+        user_choice = int(input(''))
+        print('Que souhaitez vous ?:\1)Modification de données\n2)Verification IMC\n3)Etat de santé')
+        action = int(input(''))
+        
+        if action == 1:
+            pass
+        
+    if option==2:
         pass
-    if option == 2:
+    elif action==3:
         pass
+    else:
+        print("Valeur non reconnue")
